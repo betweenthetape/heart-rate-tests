68b9910a577284b16d8298e4dbda58c47ce9c726
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2025-09-17 22:28:40 +0200

    convert m -> km

--- inst/scripts/test.R
+++ inst/scripts/test.R
@@ -4,10 +4,11 @@ test <- read_csv("inst/extdata/example.csv", skip = 2)
 
 test |>
   select(distance = "Distances (m)", hr = "HR (bpm)") |>
+  mutate(distance = distance / 1000) |>
   filter(distance != 0) |> # Prevent line coming up from zero while waiting for movement
   ggplot(aes(x = distance, y = hr)) +
   geom_line() +
   ggridges::theme_ridges() +
-  labs(x = "Distance (m)", y = "Heart Rate (bpm)")
+  labs(x = "Distance (Km)", y = "Heart Rate (bpm)")
 
 # Add hlines on key points of the trail. Use GPS/distance via map to identify where these key points are.
