cf414161c329bf38493115ef4d9d1b60cbbb98c4
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2025-09-19 19:45:52 +0200

    Add ebike data

--- inst/scripts/ebikes-make-you-f-tter.R
+++ inst/scripts/ebikes-make-you-f-tter.R
@@ -1,8 +1,25 @@
 library(tidyverse)
 
-test <- read_csv("inst/extdata/raaw-madonna-19-09-25.csv", skip = 2)
+bike <- read_csv("inst/extdata/raaw-19-09-25.csv", skip = 2) |>
+  mutate(type = "bike")
 
-test |>
+ebike <- read_csv("inst/extdata/crestline-19-09-25.csv", skip = 2) |>
+  mutate(type = "ebike")
+
+raw <- bind_rows(bike, ebike)
+
+raw |>
+  filter(type == "bike") |>
+  select(distance = "Distances (m)", hr = "HR (bpm)") |>
+  filter(distance > 20) |> # Prevent line coming up from zero while waiting for hr detection
+  mutate(distance = distance / 1000) |>
+  ggplot(aes(x = distance, y = hr)) +
+  geom_line() +
+  ggridges::theme_ridges() +
+  labs(x = "Distance (Km)", y = "Heart Rate (bpm)")
+
+raw |>
+  filter(type == "ebike") |>
   select(distance = "Distances (m)", hr = "HR (bpm)") |>
   filter(distance > 20) |> # Prevent line coming up from zero while waiting for hr detection
   mutate(distance = distance / 1000) |>
@@ -12,6 +29,7 @@ test |>
   labs(x = "Distance (Km)", y = "Heart Rate (bpm)")
 
 # TODO:
+# Plot both types on one plot by colour w/ label
 # Add hlines on key points of the trail. Use GPS/distance via map to identify where these key points are.
 # Calculate how long in each training zone. This is the key insight.
 # Show mean, min, max, summaries, but highlight how they can be misleading as what we care about is how long in each zone.
