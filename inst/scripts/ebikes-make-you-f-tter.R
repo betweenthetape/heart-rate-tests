ed901ac8fd24351f565baf40e03ace44f204bfde
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2025-09-19 17:21:47 +0200

    Replace with actual test data

--- inst/scripts/ebikes-make-you-f-tter.R
+++ inst/scripts/ebikes-make-you-f-tter.R
@@ -1,16 +1,17 @@
 library(tidyverse)
 
-test <- read_csv("inst/extdata/example.csv", skip = 2)
+test <- read_csv("inst/extdata/raaw-madonna-19-09-25.csv", skip = 2)
 
 test |>
   select(distance = "Distances (m)", hr = "HR (bpm)") |>
+  filter(distance > 20) |> # Prevent line coming up from zero while waiting for hr detection
   mutate(distance = distance / 1000) |>
-  filter(distance != 0) |> # Prevent line coming up from zero while waiting for movement
   ggplot(aes(x = distance, y = hr)) +
   geom_line() +
   ggridges::theme_ridges() +
   labs(x = "Distance (Km)", y = "Heart Rate (bpm)")
 
+# TODO:
 # Add hlines on key points of the trail. Use GPS/distance via map to identify where these key points are.
-# Calculate how long in each training zone.
-# Show mean, min, max, summaries
+# Calculate how long in each training zone. This is the key insight.
+# Show mean, min, max, summaries, but highlight how they can be misleading as what we care about is how long in each zone.
