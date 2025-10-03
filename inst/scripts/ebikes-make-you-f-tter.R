79189bef8428ebc0142f047341108412457a38e1
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2025-10-03 15:19:55 +0200

    Export table

--- inst/scripts/ebikes-make-you-f-tter.R
+++ inst/scripts/ebikes-make-you-f-tter.R
@@ -35,6 +35,17 @@ ggsave(
   dpi = 330
 )
 
+ggsave(
+  "inst/scripts/assets/plot_transparent_bike.png",
+  plot = plot_bike,
+  width = 2200,
+  height = 1800,
+  units = "px",
+  bg = "transparent",
+  limitsize = FALSE,
+  dpi = 330
+)
+
 plot_ebike <- raw |>
   filter(type == "ebike") |>
   select(distance = "Distances (m)", hr = "HR (bpm)") |>
@@ -57,6 +68,17 @@ ggsave(
   height = 1800,
   units = "px",
   bg = "white",
+  limitsize = FALSE,
+  dpi = 330
+)
+
+ggsave(
+  "inst/scripts/assets/plot_transparent_ebike.png",
+  plot = plot_ebike,
+  width = 2200,
+  height = 1800,
+  units = "px",
+  bg = "transparent",
   limitsize = FALSE,
   dpi = 330
 )
@@ -88,21 +110,25 @@ plot_both <- raw |>
   )
 
 ggsave(
-  "inst/scripts/assets/plot_both.png",
+  "inst/scripts/assets/plot_transparent_both.png",
   plot = plot_both,
   width = 2200,
   height = 1800,
   units = "px",
-  bg = "white",
+  bg = "transparent",
   limitsize = FALSE,
   dpi = 330
 )
 
-raw |>
+threshold <- raw |>
   select(distance = "Distances (m)", hr = "HR (bpm)", type) |>
-  mutate(below_threshold = if_else(hr < 130, TRUE, FALSE)) |>
+  mutate(gap = distance - lag(distance, default = 0), .by = type) |>
+  mutate(below_threshold = if_else(hr < 130, TRUE, FALSE))
+
+table_threshold <- threshold |>
   summarise(
-    perc_below_threshold = sum(below_threshold) / n(),
+    time_below_threshold_percentage = mean(below_threshold),
+    distance_below_threshold = sum(gap * below_threshold) / 1000,
     .by = type
   ) |>
   gt(rowname_col = "type") |>
@@ -113,6 +139,34 @@ raw |>
   ) |>
   cols_label(
     type = "",
-    perc_below_threshold = "Time below threshold"
+    time_below_threshold_percentage = "Time below threshold",
+    distance_below_threshold = "Distance below treshold"
   ) |>
-  fmt_percent(columns = perc_below_threshold)
+  fmt_percent(
+    columns = time_below_threshold_percentage,
+    decimals = 1
+  ) |>
+  fmt_number(
+    columns = distance_below_threshold,
+    decimals = 1,
+    pattern = "{x} km"
+  ) |>
+  tab_header(
+    md(
+      "Twice as much time was spent above the zone 2 (130 BPM)<br>threshold on the normal bike compared to the ebike"
+    )
+  ) |>
+  tab_style(
+    style = cell_text(color = "#541352FF", weight = "bold"),
+    locations = cells_stub(rows = type == "bike")
+  ) |>
+  tab_style(
+    style = cell_text(color = "#ffb10c", weight = "bold"),
+    locations = cells_stub(rows = type == "ebike")
+  )
+
+gtsave(
+  table_threshold,
+  "inst/scripts/assets/table_treshold.png",
+  zoom = 10
+)
